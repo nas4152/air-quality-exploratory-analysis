30a102b27a1159ff024e5a3489e1f68ffd2e7e24
nas4152 <nas4152@gmail.com>
2016-07-29 19:19:09 -0400

    finished plot 1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Air Quality Exploratory Analysis
+## Air Quality Exploratory Analysis
+
+# Plot 1
+
+Plot 1 explores the change in total reported PM2.5 emissions from 1999 to 2008.
+Sums were calculated for the years 1999, 2002, 2005, and 2008 and plotted with 
+a regression line.  It should be noted that despite an increase in reported 
+values for each year sampled, the total reported emissions still decreased.
+
--- plot1.R
+++ plot1.R
@@ -13,3 +13,34 @@ if (!file.exists("Source_Classification_Code.rds")) {
 #read files into R
 aqdata <- readRDS("summarySCC_PM25.rds")
 sourceids <- readRDS("Source_Classification_Code.rds")
+
+## checked for NAs in emissions data with sum(is.na(aqdata$Emissions))
+## returned 0
+
+## Have total emissions from PM2.5 decreased in the United States from 1999 to 
+## 2008? Using the base plotting system, make a plot showing the total PM2.5 
+## emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+
+##Note: total is skewed, number of observations increases over time
+##with(aqdata, tapply(Emissions, year, length))
+##1999    2002    2005    2008 
+##1108469 1698677 1713850 1976655 
+
+sums <- with(aqdata, tapply(Emissions, year, sum))
+year <- as.numeric(names(sums))
+
+png(file = "plot1.png")
+par(mar = c(10, 4, 4, 2))
+plot(year, sums, pch = 19, xlab = "Year", ylab = "Total PM2.5", 
+     main = "Total Emissions over Time")
+model <- lm(sums ~ year)
+abline(model, col = "steelblue", lwd = 2)
+mtext("Note: The total is skewed, as the number of observations increases over 
+        time
+        1999          2002          2005          2008 
+        1,108,469  |1,698,677  |1,713,850  |1,976,655", side = 1, line = 8, 
+      cex = 0.8)
+dev.off()
+
+
+
