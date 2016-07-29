3bbfd6448808e71ebbd1f43aae145d4d1158337e
nas4152 <nas4152@gmail.com>
2016-07-29 19:41:57 -0400

    finished plot2

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ plotted with a regression line.  It should be noted that despite an increase in
 reported values for each year sampled, the total reported emissions still 
 decreased.
 
+##Plot 2
+
+Plot 2 explores the same relationship, focusing on the data from Baltimore City,
+MD.  While this plot does not show a consistent decrease, the overall trend 
+remains negative.  The spike in 2005 probably cannot be explained solely by the
+increase in values reported, as there is only a 1.3% increase in values reported
+between 2002 and 2005.  This would have to be further investigated.
--- plot2.R
+++ plot2.R
@@ -13,3 +13,32 @@ if (!file.exists("Source_Classification_Code.rds")) {
 #read files into R
 aqdata <- readRDS("summarySCC_PM25.rds")
 sourceids <- readRDS("Source_Classification_Code.rds")
+
+## checked for NAs in emissions data with sum(is.na(aqdata$Emissions))
+## returned 0
+
+##Have total emissions from PM2.5 decreased in the Baltimore City, Maryland 
+##(fips == "24510") from 1999 to 2008? Use the base plotting system to make a 
+##plot answering this question.
+
+baltdata <- subset(aqdata, fips == "24510")
+sums <- with(baltdata, tapply(Emissions, year, sum))
+year <- as.numeric(names(sums))
+
+##total observations over time: with(baltdata, tapply(Emissions, year, length))
+##1999 2002 2005 2008 
+##320  535  542  699 
+png(file = "plot2.png")
+par(mar = c(10, 4, 4, 2))
+plot(year, sums, pch = 19, xlab = "Year", ylab = "Total PM2.5", 
+     main = "Total Emissions over Time in Baltimore")
+model <- lm(sums ~ year)
+abline(model, col = "steelblue", lwd = 2)
+mtext("Note: The total is skewed, as the number of observations increases over 
+      time
+      1999 | 2002 | 2005 | 2008 
+      320  | 535  | 542  | 699 ", side = 1, line = 8, 
+      cex = 0.8)
+dev.off()
+
+
